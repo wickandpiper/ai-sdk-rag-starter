7f15bf5b132622937f2a538044b925eede7136bb
dinkydigitalprojects <59416546+wickandpiper@users.noreply.github.com>
2025-02-19 23:57:34 +0000

    Initial commit

--- lib/actions/resources.ts
+++ lib/actions/resources.ts
@@ -6,6 +6,8 @@ import {
   resources,
 } from '@/lib/db/schema/resources';
 import { db } from '../db';
+import { generateEmbeddings } from '../ai/embedding';
+import { embeddings as embeddingsTable } from '../db/schema/embeddings';
 
 export const createResource = async (input: NewResourceParams) => {
   try {
@@ -16,7 +18,15 @@ export const createResource = async (input: NewResourceParams) => {
       .values({ content })
       .returning();
 
-    return 'Resource successfully created.';
+    const embeddings = await generateEmbeddings(content);
+    await db.insert(embeddingsTable).values(
+      embeddings.map(embedding => ({
+        resourceId: resource.id,
+        ...embedding,
+      })),
+    );
+
+    return 'Resource successfully created and embedded.';
   } catch (e) {
     if (e instanceof Error)
       return e.message.length > 0 ? e.message : 'Error, please try again.';
